37a7b483cbf651b3d7c1c8f6c5658aa7bb9abd8c
kielbasat <t.kielbasa@tlen.pl>
2022-03-19 17:43:17 +0100

    Added find element(s) By Id, Name, Class, Tag Name, Link text, partial link text

---
+++ src/test/java/SelectorTest.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class SelectorTest extends TestBase {
+
+    String baseUrl = "https://testeroprogramowania.github.io/selenium/basics.html";
+
+    @Test
+    public void findElementById() {
+        driver.get(baseUrl);
+        By buttonId = By.id("clickOnMe");
+        WebElement clickOnMeBtn = driver.findElement(buttonId);
+    }
+
+    @Test
+    public void findElementByName() {
+        driver.get(baseUrl);
+        By fieldName = By.name("fname");
+        driver.findElement(fieldName);
+    }
+
+    @Test
+    public void findElementByClass() {
+        driver.get(baseUrl);
+        By paragraphClassName = By.className("topSecret");
+        driver.findElement(paragraphClassName);
+    }
+
+    @Test
+    public void findElementByTagName() {
+        driver.get(baseUrl);
+        By inputLocator = By.tagName("input");
+        driver.findElement(inputLocator);
+    }
+
+    @Test
+    public void findElementsByTagName() {
+        driver.get(baseUrl);
+        By inputLocator = By.tagName("input");
+        List<WebElement> inputs = driver.findElements(inputLocator);
+        System.out.println(inputs.size());
+    }
+
+    @Test
+    public void findElementsByLinkText() {
+        driver.get(baseUrl);
+        By linkText = By.linkText("Visit W3Schools.com!");
+        driver.findElement(linkText);
+        By partialLinkText = By.partialLinkText("Visit W3Schools.com");
+        driver.findElement(partialLinkText);
+    }
+}
+
