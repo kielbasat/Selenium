9591813f930c5e5e95ca4091f0514ae8b49cd758
kielbasat <t.kielbasa@tlen.pl>
2022-03-21 11:32:16 +0100

    Added Homework with google search Css selectors

---
+++ src/test/java/HomeWorkCssSelectorsTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class HomeWorkCssSelectorsTest extends TestBase {
+
+    By cookiesAgreeBtn = By.cssSelector("#L2AGLb");
+    By luckyBtn = By.cssSelector("div.FPdoLc input.RNmpXc");
+    By searchField = By.cssSelector(".gLFyf");
+
+    @Test
+    public void googleLuckySearch(){
+        driver.get("https://www.google.pl/");
+
+        driver.findElement(cookiesAgreeBtn).click();
+        driver.findElement(searchField).sendKeys("Selenium");
+        driver.findElement(luckyBtn).click();
+
+        assertEquals(driver.getCurrentUrl(), "https://www.selenium.dev/");
+
+    }
+}
