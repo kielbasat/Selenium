2ddae0ece1b0e9adfb994387f1e40529fa528d03
kielbasat <t.kielbasa@tlen.pl>
2022-03-23 12:51:45 +0100

    Added iframe switch with WebElement

---
+++ src/test/java/JSExecutorTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class JSExecutorTest extends TestBase{
+    @Test
+    public void executeJS(){
+
+        String baseUrl = "https://testeroprogramowania.github.io/selenium/";
+        driver.get(baseUrl);
+        WebElement basePageLink = driver.findElement(By.linkText("Podstawowa strona testowa"));
+
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click()", basePageLink);
+
+        WebElement firstName = driver.findElement(By.name("fname"));
+        executor.executeScript("arguments[0].setAttribute('value', 'Bartek')", firstName);
+    }
+}
