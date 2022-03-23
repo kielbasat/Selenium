2c31e9a93f799d209cac3bd80e4b9bce56c8f682
kielbasat <t.kielbasa@tlen.pl>
2022-03-23 14:07:28 +0100

    Added few actions which can be performed

---
+++ src/test/java/ActionTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ActionTest extends TestBase{
+
+    //By twiceClickBtn = By.cssSelector("#bottom");
+
+
+    @Test
+    public void ActionPerform(){
+
+
+        Actions actions = new Actions(driver);
+        //driver.get("https://testeroprogramowania.github.io/selenium/doubleclick.html");
+        //actions.contextClick().perform();
+        //actions.contextClick(driver.findElement(twiceClickBtn)).perform();
+        //actions.doubleClick(driver.findElement(twiceClickBtn)).perform();
+
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        actions.moveToElement(driver.findElement(By.tagName("h1"))).perform();
+    }
+}
