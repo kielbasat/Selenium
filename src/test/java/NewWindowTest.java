bb5cda438f5e8f3a7fef3a11158bde7671d65d5d
kielbasat <t.kielbasa@tlen.pl>
2022-03-23 11:11:32 +0100

    Added New window tests

---
+++ src/test/java/NewWindowTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class NewWindowTest extends TestBase{
+
+    By clickMeBtnCss = By.cssSelector("#newPage");
+    By cookiesBtn = By.cssSelector("#L2AGLb .QS5gu");
+    By inputSearchField = By.cssSelector("input.gLFyf");
+    By firstNameInput = By.cssSelector("#fname");
+
+    @Test
+    public void newWindow(){
+        String baseUrl = "https://testeroprogramowania.github.io/selenium/basics.html";
+
+        driver.get(baseUrl);
+        String currentWindowName = driver.getWindowHandle();
+        driver.findElement(clickMeBtnCss).click();
+
+        Set<String> windowNames = driver.getWindowHandles();
+        for(String windowName : windowNames) {
+            if(windowName != currentWindowName)
+                driver.switchTo().window(windowName);
+        }
+        driver.findElement(cookiesBtn).click();
+        driver.findElement(inputSearchField).sendKeys("Selenium");
+        driver.switchTo().window(currentWindowName);
+        driver.findElement(firstNameInput).sendKeys("Tomek");
+
+
+
+
+
+
+
+
+    }
+}
