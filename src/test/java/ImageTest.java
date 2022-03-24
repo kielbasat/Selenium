d7d59683b8c80ae3388bfae72ddf238400bf6313
kielbasat <t.kielbasa@tlen.pl>
2022-03-24 18:13:14 +0100

    Added image presence checking test

---
+++ src/test/java/ImageTest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ImageTest extends TestBase {
+    @Test
+    public void imageExist() {
+        driver.get("https://testeroprogramowania.github.io/selenium/image.html");
+        WebElement image = driver.findElement(By.cssSelector("#smileImage"));
+        String imageHigh = image.getAttribute("naturalHeight");
+
+        Assert.assertEquals(imageHigh, "0");
+
+    }
+}
